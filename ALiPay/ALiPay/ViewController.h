b1edd4fa19dd2cb77180fb77c56291a62d5f6c17
senhowe <389889154@qq.com>
2016-01-12 18:01:21 +0800

    no message

---
+++ ALiPay/ALiPay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ALiPay
+//
+//  Created by wey on 16/1/12.
+//  Copyright © 2016年 wey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
