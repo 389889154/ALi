957953652faea307561f5030d20e20a99129a540
senhowe <389889154@qq.com>
2016-01-12 16:44:22 +0800

    no message

---
+++ wey_UI/wey_UI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  wey_UI
+//
+//  Created by wey on 16/1/12.
+//  Copyright © 2016年 wey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ wey_UI/wey_UI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  wey_UI
+//
+//  Created by wey on 16/1/12.
+//  Copyright © 2016年 wey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
